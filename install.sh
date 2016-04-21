fafd31cc0913dc2cc6e2f3e9039a0fa4599b114d
Lasse S. Haslev <lasse@haslev.no>
2016-04-21 23:26:07 +0200

    First commit

---
+++ install.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# Generate the ssh key
+ssh-keygen -t rsa
+
+# --- Install Programs --- #
+# Setup homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Then install homebrew cask
+brew tap caskroom/cask
+brew install caskroom/cask/brew-cask
+
+# Install git first because cask needs git
+brew install git # Git repositories
+brew install npm # Package manager for javascript
+
+brew cask install google-chrome # Web browser
+brew cask install adium # Communication software
+brew cask install skype # Communication software
+brew cask install reeder # News reader
+brew cask install vlc # Video player
+brew cask install spotify # Music streaming
+brew cask install dropbox # SkyDrive file manager
+
+brew cask install dash # Gode documentation
+brew cask iterm2 # A better terminal for Mac
+brew cask alfred # A better terminal for Mac
+brew cask caffeine # Prevent mac from sleeping
+
+brew cask vagrant # Virtual mashine
+brew cask virtualbox # Prevent mac from sleeping
+
+brew cask slack # Team communication
+brew cask 1password # Password manager
+
+brew cask mindnode-pro # Mind mapping software
+brew cask macvim # VIM Text editor for mac
+brew cask moom # Mac window organization
+
+brew cask transmit # FTP client
+
+brew cask sequel-pro # Graphical SQL client
+brew cask livereload # Reloads browser when you change your files developing web apps
+brew cask rescuetime # Monitor your activities
+brew cask graphviz # Write graphs in dot
+brew cask textwrangler # Texteditor with GREP posibilities
+
+# Setup vim rc file
+git clone -b personal https://github.com/LasseHaslev/ultimate-vimrc.git ~/.vim/runtime
+sh ~/.vim/runtime/install.sh
