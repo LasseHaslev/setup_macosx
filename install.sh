e73ae0b3f6398d68b5ab8e59ebbeeef87500c9db
Lasse S. Haslev <lasse@haslev.no>
2016-05-20 20:52:08 +0200

    Removed applications

--- install.sh
+++ install.sh
@@ -12,7 +12,6 @@ brew install caskroom/cask/brew-cask
 brew cask install google-chrome # Web browser
 brew cask install franz # Social messenger software
 brew cask install skype # Communication software
-brew cask install vlc # Video player
 brew cask install spotify # Music streaming
 brew cask install dropbox # SkyDrive file manager
 
@@ -20,10 +19,6 @@ brew cask install alfred # A better terminal for Mac
 brew cask install caffeine # Prevent mac from sleeping
 
 brew cask install 1password # Password manager
-
-brew cask install moom # Mac window organization
-
-brew cask install nylas-n1 # Mail client
 
 # A fun little detail when the script has been finished
 say -v whisper "It has been done"
